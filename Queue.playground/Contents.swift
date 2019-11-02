868b41272543a9430e1a33c34b9f57500bc3be52
cooo002 <cooo002@naver.com>
2019-11-02 15:10:37 +0900

    Queue

---
+++ Queue.playground/Contents.swift
@@ -0,0 +1,64 @@
+public struct Queue<T>{
+    private var data = [T]()
+    
+    public init(){}
+    
+    public mutating func dequeue() -> T?{
+        return self.data.removeFirst()
+        
+    }
+    
+    public mutating func peek() -> T? {
+        return self.data.first
+    }
+    
+    public mutating func enqueue(element : T){
+        self.data.append(element)
+    }
+    
+    public mutating func clear(){
+        self.data.removeAll()
+    }
+    
+    public var count : Int{
+        return self.data.count
+    }
+    
+    public var capacity : Int{
+        get{
+        return self.data.capacity
+    }
+        set
+        {
+            self.data.reserveCapacity(newValue)
+        }
+    
+    }
+    public func isFull() -> Bool{
+        if  data.count == data.capacity{
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    
+    public func isEmpty() -> Bool {
+        return data.isEmpty
+    }
+}
+    
+
+var queueEx = Queue<Int>()
+
+queueEx.enqueue(element: 10)
+queueEx.enqueue(element: 3)
+queueEx.enqueue(element: 4)
+queueEx.enqueue(element: 6)
+
+queueEx.peek()
+
+
+
+
+
